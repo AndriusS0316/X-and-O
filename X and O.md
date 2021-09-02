dbad3dcf822db5119478dfdf38ce67acb1711c50
Andrius <andriuss0316@gmail.com>
2021-09-02 13:33:06 +0300

    20210902

---
+++ X and O.md
@@ -0,0 +1,1 @@
+Padaryti žaidimą "X and O". Pagal tradicines taisykles. Taip pat sugeneruoti mygtukus, kurie leis pasirinkti kuris žaidėjas eis pirmas. Ir atskiru mygtuku suteikti žaidėjams galimybę pasirinkti, "random" būdu, kuris žaidėjas eis pirmas. 
---
+++ main.js
@@ -0,0 +1,35 @@
+
+let rez = 0;
+let random = 0;
+
+
+
+const skaicius = document.querySelector('.skaicius');
+const change = document.querySelector('.change');
+const number = document.querySelector('.number');
+
+
+
+skaicius.innerText 
+number.innerText
+
+
+
+
+function changeClick() {
+    
+}
+
+
+
+function numberClick() {
+    
+}
+
+
+
+change.addEventListener('click', changeClick);
+
+number.addEventListener('click', numberClick);
+
+skaicius.addEventListener('click', skaiciusClick);
