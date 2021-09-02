006cfdc00dc33904691b33676079fd61a38f906b
Andrius <andriuss0316@gmail.com>
2021-09-02 14:53:48 +0300

    20210902

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 let rez = 0;
 let randomm;
+let patikra = false;
 
 
 
@@ -23,26 +24,40 @@ const devyni = document.querySelector('.devyni');
 
 
 
-title.innerText 
+title.innerText
 
 
 let deactivateRandom = document.querySelector('.random');
 let deactivateX = document.querySelector('.x');
 let deactivateO = document.querySelector('.o');
 
+let deactivateVienas = document.querySelector('.vienas');
+let deactivateDu = document.querySelector('.du');
+let deactivateTrys = document.querySelector('.trys');
+
+let deactivateKeturi = document.querySelector('.keturi');
+let deactivatePenki = document.querySelector('.penki');
+let deactivateSesi = document.querySelector('.sesi');
+
+let deactivateSeptyni = document.querySelector('.septyni');
+let deactivateAstuoni = document.querySelector('.astuoni');
+let deactivateDevyni = document.querySelector('.devyni');
+
 
 
 function randomClick() {
     rez = (Math.floor(Math.random() * 10) + 1);
 
-    if(rez % 2 == 0) {
+    if (rez % 2 == 0) {
         title.innerText = "X Move";
         console.log("x");
         randomm = 2;
+        patikra = true;
     } else {
         title.innerText = "O Move";
         console.log("o");
         randomm = 3;
+        patikra = true;
     }
     document.querySelector('.random').disabled = 'true';
     document.querySelector('.x').disabled = 'true';
@@ -64,6 +79,8 @@ function xClick() {
     deactivateRandom.style.background = 'grey';
     deactivateX.style.background = 'grey';
     deactivateO.style.background = 'grey';
+
+    patikra = true;
 }
 
 
@@ -77,17 +94,76 @@ function oClick() {
     deactivateRandom.style.background = 'grey';
     deactivateX.style.background = 'grey';
     deactivateO.style.background = 'grey';
+
+    patikra = true;
 }
 
 
 function vienasClick() {
-    console.log(1);
+    if (patikra) {
+        console.log(1);
+        if (randomm % 2 == 0) {
+            document.querySelector('.vienas').disabled = 'true';
+            deactivateVienas.style.background = 'blue';
+            deactivateVienas.style.color = 'red';
+            title.innerText = "O Move"
+            vienas.innerText = "X"
+            randomm = randomm + 1;
+        } else {
+            document.querySelector('.vienas').disabled = 'true';
+            deactivateVienas.style.background = 'red';
+            deactivateVienas.style.color = 'blue';
+            title.innerText = "X Move"
+            vienas.innerText = "O"
+            randomm = randomm + 1;
+        }
+    } else {
+        alert("Pirma pasirinkit, kieno ėjimas bus pirmas");
+    }
 }
 function duClick() {
-    console.log(2);
+    if (patikra) {
+        console.log(2);
+        if (randomm % 2 == 0) {
+            document.querySelector('.du').disabled = 'true';
+            deactivateDu.style.background = 'blue';
+            deactivateDu.style.color = 'red';
+            title.innerText = "O Move"
+            du.innerText = "X"
+            randomm = randomm + 1;
+        } else {
+            document.querySelector('.du').disabled = 'true';
+            deactivateDu.style.background = 'red';
+            deactivateDu.style.color = 'blue';
+            title.innerText = "X Move"
+            du.innerText = "O"
+            randomm = randomm + 1;
+        }
+    } else {
+        alert("Pirma pasirinkit, kieno ėjimas bus pirmas");
+    }
 }
 function trysClick() {
-    console.log(3);
+    if (patikra) {
+        console.log(3);
+        if (randomm % 2 == 0) {
+            document.querySelector('.trys').disabled = 'true';
+            deactivateTrys.style.background = 'blue';
+            deactivateTrys.style.color = 'red';
+            title.innerText = "O Move"
+            trys.innerText = "X"
+            randomm = randomm + 1;
+        } else {
+            document.querySelector('.trys').disabled = 'true';
+            deactivateTrys.style.background = 'red';
+            deactivateTrys.style.color = 'blue';
+            title.innerText = "X Move"
+            trys.innerText = "O"
+            randomm = randomm + 1;
+        }
+    } else {
+        alert("Pirma pasirinkit, kieno ėjimas bus pirmas");
+    }
 }
 function keturiClick() {
     console.log(4);
